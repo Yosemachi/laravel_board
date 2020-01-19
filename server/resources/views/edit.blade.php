961a7bca4ab9ab9870d8fcbc3240cafaed2c6646
yosemachi <yosemati1997@gmail.com>
2020-01-20 05:25:06 +0900

    画面調整した

--- server/resources/views/edit.blade.php
+++ server/resources/views/edit.blade.php
@@ -1,18 +1,29 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 @section('content')
 
-<h1>投稿編集</h1>
-{{Form::open(['action' => 'boardController@save', 'method' => 'POST'])}}
-{{ csrf_field() }}
-{{Form::hidden('id', $board['id'])}}
 
-{{Form::textarea('message', $board['message'] )}}
-
-{{Form::submit('更新')}}
-{{ Form::close() }}
-{{Form::open(['action' => 'boardController@delete', 'method' => 'POST'])}}
-{{ csrf_field() }}
-{{Form::hidden('id', $board['id'])}}
-{{Form::submit('削除')}}
-{{ Form::close() }}
+<div class="form-group row justify-content-center">
+    <div>
+        <h1>投稿編集</h1>
+        {{Form::open(['action' => 'boardController@save', 'method' => 'POST'])}}
+        {{ csrf_field() }}
+        {{Form::hidden('id', $board['id'])}}
+        <div class="form-group">
+            {{Form::textarea('message', $board['message'], ['class' => 'form-control'] )}}
+        </div>
+        <div class="row justify-content-center">
+            <div class="btn-group m-1">
+                {{Form::submit('更新', ['class' => 'btn btn-primary'])}}
+            </div>
+            {{ Form::close() }}
+            {{Form::open(['action' => 'boardController@delete', 'method' => 'POST'], ['class' => 'w-25'])}}
+            {{ csrf_field() }}
+            {{Form::hidden('id', $board['id'])}}
+            <div class="btn-group m-1">
+                {{Form::submit('削除', ['class' => 'btn btn-primary'])}}
+            </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+</div>
 @endsection
--- server/resources/views/index.blade.php
+++ server/resources/views/index.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 @section('content')
 
-<h1>Laravel掲示板</h1>
 {{Form::open(['action' => 'boardController@create', 'method' => 'POST'])}}
 {{ csrf_field() }}
-
-{{Form::text('message')}}
-
-{{Form::submit('送信')}}
+<div class="mx-auto w-75">
+    <h1>Laravel掲示板</h1>
+    <div class="form-group mx-auto">
+        {{Form::text('message', null, ['class' => 'form-control'])}}
+        <div class="col text-center">
+            {{Form::submit('送信', ['class' => 'btn btn-primary m-1 w-25'])}}
+        </div>
+    </div>
+</div>
 {{ Form::close() }}
-<table>
+<table class="table table-striped mx-auto w-75 m-5">
     @foreach($board as $key=>$value)
     <tr>
         <th>{{$value['name']}}</th>
-        <td>{{$value['message']}}</td>
-        <td><button><a href="/edit/{{ $value['id'] }}">編集</a></button></td>
+        <td class="">{{$value['message']}}</td>
+        <td><button class="btn btn-primary"><a href="/edit/{{ $value['id'] }}" class=" text-light">編集</a></button></td>
     </tr>
     @endforeach
 </table>
